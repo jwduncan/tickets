feb89fa23584f67fe7de30a8ddb6f95ad7b7690b
Jeff Duncan <duncancf24@yahoo.com>
2013-08-14 22:39:48 -0400

    Added phone number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130815001054) do
+ActiveRecord::Schema.define(version: 20130815022501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20130815001054) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
 end
